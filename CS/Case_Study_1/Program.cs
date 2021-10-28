71716d36fc944fd10c8640b8ef0ad82e109e1c3b
Wongvarit Pancharoen <oakbahina@gmail.com>
2021-10-28 15:31:44 +0700

    init

--- CS/Case_Study_1/Program.cs
+++ CS/Case_Study_1/Program.cs
@@ -10,8 +10,10 @@ namespace Problem01
     class Program
     {
         static byte[] Data_Global = new byte[1000000000];
-        static long Sum_Global = 0;
+        static long[] Sum_Global = new long[4];
         static int G_index = 0;
+        static Thread t1,t2,t3,t4;
+        // 30667ms
 
         static int ReadData()
         {
@@ -35,31 +37,43 @@ namespace Problem01
 
             return returnData;
         }
-        static void sum()
+        static void sum(ref long psum, int idx)
         {
-            if (Data_Global[G_index] % 2 == 0)
+            // time reduced by changed data ref method
+            int val = Data_Global[idx];
+            if (val % 2 == 0)
             {
-                Sum_Global -= Data_Global[G_index];
+                psum -= val;
             }
-            else if (Data_Global[G_index] % 3 == 0)
+            else if (val % 3 == 0)
             {
-                Sum_Global += (Data_Global[G_index]*2);
+                psum += (val * 2);
             }
-            else if (Data_Global[G_index] % 5 == 0)
+            else if (val % 5 == 0)
             {
-                Sum_Global += (Data_Global[G_index] / 2);
+                psum += (val / 2);
             }
-            else if (Data_Global[G_index] %7 == 0)
+            else if (val % 7 == 0)
             {
-                Sum_Global += (Data_Global[G_index] / 3);
+                psum += (val / 3);
             }
-            Data_Global[G_index] = 0;
-            G_index++;   
+            Data_Global[idx] = 0;
         }
+
+        static void task(ref long psum,int start,int stop){
+                for(int i = start;i < stop;++i)
+                {
+                     sum(ref psum,i);
+                }
+        }
+
+
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
-            int i, y;
+            int y;
+            const long correct = 888701676;
+            const int  approxDefaultRuntime = 30000;
 
             /* Read data from file */
             Console.Write("Data read...");
@@ -73,17 +87,54 @@ namespace Problem01
                 Console.WriteLine("Read Failed!");
             }
 
+            var cores = 4;
+            var divider = 1000000000/cores;
+
+            // var t_array = new Thread[cores];
+
+            t1 = new Thread(() => {
+                task(ref Sum_Global[0],0,divider);
+            });
+
+            t2 = new Thread(() => {
+                task(ref Sum_Global[1],divider,divider * 2);
+            });
+
+            t3 = new Thread(() => {
+                task(ref Sum_Global[2],divider * 2 ,divider * 3);
+            });
+
+            t4 = new Thread(() => {
+                task(ref Sum_Global[3],divider * 3 ,divider * 4);
+            });
+         
+
             /* Start */
             Console.Write("\n\nWorking...");
             sw.Start();
-            for (i = 0; i < 1000000000; i++)
-                sum();
+
+          
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+
+
             sw.Stop();
             Console.WriteLine("Done.");
 
             /* Result */
-            Console.WriteLine("Summation result: {0}", Sum_Global);
+            long s = 0;
+            Array.ForEach(Sum_Global, i => s += i);
+            Console.WriteLine("Summation result: {0}", s);
             Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+            Console.WriteLine("Time Reduced: {0}%", (1 - (sw.ElapsedMilliseconds * 1.0)/(approxDefaultRuntime * 1.0)).ToString("N2") );
+            Console.WriteLine("Summation Correctness: {0}", s == correct);
         }
     }
 }
---
+++ Sync/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Sync
+{
+    
+    class Program
+    {
+        const int iterlation_size = 1000001;
+
+        static void Main(string[] args)
+        {
+            //  Ex00.run();
+            //  Ex01.run();
+            //  Ex02.run();
+            //  Ex03.run();
+            // Ex04.run();
+            Ex05.run();
+        }
+
+        public class Ex00{
+
+            private static int sum = 0;
+
+            static void plus(){
+                int i;
+                for(i = 1;i < iterlation_size; ++i){
+                    sum += i;
+                }
+            }
+
+            static void minus(){
+                int i;
+                for(i = 1;i < iterlation_size; ++i){
+                    sum -= i;
+                }
+            }
+
+            public static void run(){
+                Stopwatch sw = new Stopwatch();
+                Console.WriteLine("Start....");
+                sw.Start();
+                plus();
+                minus();
+                sw.Stop();
+                Console.WriteLine("sum = {0}",sum);
+                Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+            }
+
+        }
+
+        public class Ex01{
+
+            private static int sum = 0;
+
+            static void plus(){
+                int i;
+                for(i = 1;i < iterlation_size; ++i){
+                    sum += i;
+                }
+            }
+
+            static void minus(){
+                int i;
+                for(i = 1;i < iterlation_size; ++i){
+                    sum -= i;
+                }
+            }
+
+            public static void run(){
+
+                Thread P = new Thread(new ThreadStart(plus));
+                Thread M = new Thread(new ThreadStart(minus));
+
+
+                Stopwatch sw = new Stopwatch();
+                Console.WriteLine("Start....");
+                sw.Start();
+
+                P.Start();
+                M.Start();
+
+                P.Join();
+                M.Join();
+
+                sw.Stop();
+                Console.WriteLine("sum = {0}",sum);
+                Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+            }
+
+        }
+
+        public class Ex02{
+
+            private static int sum = 0;
+            private static object _Lock = new object();
+
+            static void plus(){
+                int i;
+                for(i = 1;i < iterlation_size; ++i){
+                    lock(_Lock)
+                    sum += i;
+                
+                }
+            }
+
+            static void minus(){
+                int i;
+                for(i = 1;i < iterlation_size; ++i){
+                    lock(_Lock)
+                    sum -= i;
+                }
+            }
+
+            public static void run(){
+
+                Thread P = new Thread(new ThreadStart(plus));
+                Thread M = new Thread(new ThreadStart(minus));
+
+
+                Stopwatch sw = new Stopwatch();
+                Console.WriteLine("Start....");
+                sw.Start();
+
+                P.Start();
+                M.Start();
+
+                P.Join();
+                M.Join();
+
+                sw.Stop();
+                Console.WriteLine("sum = {0}",sum);
+                Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+            }
+
+        }
+
+        public class Ex03{
+
+            private static int sum = 0;
+            private static object _Lock = new object();
+
+            static void plus(){
+                int i;
+                lock(_Lock)
+                for(i = 1;i < iterlation_size; ++i){
+                    
+                    sum += i;
+                
+                }
+            }
+
+            static void minus(){
+                int i;
+                lock(_Lock)
+                for(i = 1;i < iterlation_size; ++i){
+                    sum -= i;
+                }
+            }
+
+            public static void run(){
+
+                Thread P = new Thread(new ThreadStart(plus));
+                Thread M = new Thread(new ThreadStart(minus));
+
+
+                Stopwatch sw = new Stopwatch();
+                Console.WriteLine("Start....");
+                sw.Start();
+
+                P.Start();
+                M.Start();
+
+                P.Join();
+                M.Join();
+
+                sw.Stop();
+                Console.WriteLine("sum = {0}",sum);
+                Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
+            }
+
+        }
+    
+    
+        public class Ex04 {
+            private static string x = "";
+            private static int exitflag = 0;
+            private static object _Lock = new object();
+
+            static void ThReadX(){
+                lock(_Lock){
+                    while(exitflag == 0){
+                        Monitor.Wait(_Lock);
+                        if(exitflag == 1) break;
+                        Console.WriteLine("X = {0}",x);
+                    }
+                    Console.WriteLine("Thread 1 exit");
+                }
+            }
+
+            static void  ThWriteX(){
+                string xx;
+                while(exitflag == 0){
+                    lock(_Lock){
+                        Monitor.Pulse(_Lock);
+                        Console.Write("Input: ");
+                        xx = Console.ReadLine();
+                        if(xx == "exit")
+                            exitflag = 1;
+                        else
+                            x = xx;
+                    }
+                }
+            }
+
+            public static void run(){
+                Thread A = new Thread(ThReadX);
+                Thread B = new Thread(ThWriteX);
+
+                A.Start();
+                B.Start();
+            }
+        }
+    
+        public class Ex05 {
+            private static string x = "";
+            private static int exitflag = 0;
+            private static object _Lock = new object();
+
+            static void ThReadX(object i){
+                lock(_Lock){
+                    while(exitflag == 0){
+                        Monitor.Wait(_Lock);
+                        if(exitflag == 1) break;
+                        Console.WriteLine("**Thraed {0} : X = {1}**",i,x);
+                    }
+                    Console.WriteLine("---Thread {0} exit---",i);
+                }
+            }
+
+            static void  ThWriteX(){
+                string xx;
+                while(exitflag == 0){
+                    lock(_Lock){
+                        Monitor.Pulse(_Lock);
+                        Console.Write("Input: ");
+                        xx = Console.ReadLine();
+                        if(xx == "exit"){
+                            exitflag = 1;
+                            Monitor.PulseAll(_Lock);
+                        }
+                        else
+                            x = xx;
+                    }
+                }
+            }
+
+            public static void run(){
+                Thread A = new Thread(ThWriteX);
+                Thread B = new Thread(ThReadX);
+                Thread C = new Thread(ThReadX);
+                Thread D = new Thread(ThReadX);
+
+                A.Start();
+                B.Start(1);
+                C.Start(2);
+                D.Start(3);
+            }
+        }
+    }
+ 
+}
